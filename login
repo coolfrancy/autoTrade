1b4b19edf19ed8684d64674a40f059858f1dcae8
coolfrancy <154463480+coolfrancy@users.noreply.github.com>
2025-09-22 22:55:40 +0000

    added callback

---
+++ login
@@ -0,0 +1,1 @@
+https://api.schwabapi.com/v1/oauth/authorize?response_type=code&client_id=YOUR_APP_KEY&redirect_uri=https%3A%2F%2Fautotrade-production-2561.up.railway.app%2F
--- web/app.py
+++ web/app.py
@@ -1,14 +1,36 @@
-from flask import Flask
-
+from flask import Flask, request
+import os
+from dotenv import load_dotenv
+import requests
 
 #initialize flask app
 app = Flask(__name__)
-app.secret_key = 'gobrr'
+
+#get env variables
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+@app.route("/", methods=["GET"])
+def callback_root():
+    code = request.args.get("code")
+    if not code:
+        return "No ?code=... in query string", 400
 
 
-@app.route('/')
-def home():
-    return "Hello World"
+    resp = requests.post(
+        "https://api.schwabapi.com/v1/oauth/token",
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data={
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": "https://autotrade-production-2561.up.railway.app/",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+        timeout=30,
+    )
+    return resp.json()
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
